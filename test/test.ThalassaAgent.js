26ffdde2216e152e696e7a6ba3b0ebd6650aab9e
Mike Brevoort <mike.brevoort@pearson.com>
2013-08-29 17:13:31 -0600

    Fixed tests, passing more stats

--- test/test.ThalassaAgent.js
+++ test/test.ThalassaAgent.js
@@ -16,10 +16,11 @@ describe ('ThalassaAgent', function () {
       portfinder.getPort(function (err, aPort) {
         assert.ifError(err);
         port = aPort;
+        apiport = port + 1000;  // TODO ensure unique
 
-        server = new Thalassa.Server({ host: host, port: port });
-        client = new Thalassa.Client({ host: host, port: port });
-        client.register(role, version, clientPort);
+        server = new Thalassa.Server({ host: host, port: port, apiport: apiport, reaperFreq: 100 });
+        client = new Thalassa.Client({ host: host, port: port, apiport: apiport });
+        client.register(role, version, clientPort, { secondsToExpire: 1 });
         done();
       });
 
@@ -31,17 +32,17 @@ describe ('ThalassaAgent', function () {
 
     it ('should register/free add/remove members', function (done) {
       var data = new Data();
-      var agent = new ThalassaAgent({ data: data, host: host, port: port });
+      var agent = new ThalassaAgent({ data: data, host: host, port: port, apiport: apiport });
 
       data.setBackend({ name: backendName, type: 'spindrift', role: role, version: version });
-      client.connect();
+      client.start();
 
       setTimeout(function() {
         var backend = data.backends.get(data.backendId(backendName));
         assert(backend);
         var members = backend.toJSON().members;
         assert.equal(members.length, 1);
-        assert.equal(members[0].role, role);
+        assert.equal(members[0].name, role);
         assert.equal(members[0].version, version);
         client.unregister(role, version, clientPort);
 
@@ -51,7 +52,7 @@ describe ('ThalassaAgent', function () {
           var members = backend.toJSON().members;
           assert.equal(members.length, 0);
           done();
-        }, 50);
+        }, 1500);
       }, 50);
     });
   });
@@ -59,7 +60,7 @@ describe ('ThalassaAgent', function () {
   describe ('existing service registraions', function (done) {
     var port, server, client, agent, data,
         host = '127.0.0.1', backendName = 'be1',
-        role = 'foo', version = '1.1.1', clientPort = '80';
+        role = 'foo', version = '1.1.2', clientPort = '80';
 
     before(function (done) {
       data = new Data();
@@ -67,15 +68,17 @@ describe ('ThalassaAgent', function () {
       portfinder.getPort(function (err, aPort) {
         assert.ifError(err);
         port = aPort;
+        apiport = port + 1000;  // TODO ensure unique
 
-        server = new Thalassa.Server({ host: host, port: port });
-        agent = new ThalassaAgent({ data: data, host: host, port: port });
-        client = new Thalassa.Client({ host: host, port: port });
-        client.register(role, version, clientPort);
-        client.connect();
-        client.on('register', function (service) {
-          if (service.role === role && service.version === version) setTimeout(done, 10);
-        });
+        server = new Thalassa.Server({ host: host, port: port, apiport: apiport, reaperFreq: 100 });
+        agent = new ThalassaAgent({ data: data, host: host, port: port, apiport: apiport });
+        client = new Thalassa.Client({ host: host, port: port, apiport: apiport, updateFreq: 100 });
+        client.register(role, version, clientPort, { secondsToExpire: 1 });
+        client.start();
+        setTimeout(done, 500);
+        // client.on('register', function (service) {
+        //   if (service.role === role && service.version === version) setTimeout(done, 10);
+        // });
       });
     });
 
@@ -92,7 +95,7 @@ describe ('ThalassaAgent', function () {
         assert(backend);
         var members = backend.toJSON().members;
         assert.equal(members.length, 1);
-        assert.equal(members[0].role, role);
+        assert.equal(members[0].name, role);
         assert.equal(members[0].version, version);
         client.unregister(role, version, clientPort);
 
@@ -102,7 +105,7 @@ describe ('ThalassaAgent', function () {
           var members = backend.toJSON().members;
           assert.equal(members.length, 0);
           done();
-        }, 50);
+        }, 1500);
       }, 50);
     });
   });
